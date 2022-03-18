507fea7be9340c3a6fa11bc3cf56212848ebc569
noko1024 <ynoko1024@gmail.com>
2022-03-18 09:57:22 +0900

    ステータスコードを適切な値へ変更 一部リファクタリング shell補完ファイル生成(comp)を追加

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,84 +1,58 @@
 extern crate clap;
 extern crate crypto;
 extern crate aesstream;
+extern crate bcrypt;
 extern crate reqwest;
 extern crate tokio;
-extern crate rand;
 extern crate base64;
+extern crate clap_complete;
 
-use clap::{Command,Arg};
 use aesstream::{AesWriter, AesReader};
 use crypto::aessafe::{AesSafe256Encryptor, AesSafe256Decryptor};
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
+use bcrypt::hash;
 use std::io::{Read,Write,Cursor};
 use std::process;
+use std::io;
+use clap_complete::{generate, shells::Bash,shells::Elvish,shells::Fish,shells::PowerShell,shells::Zsh};
+
+mod cli;
 
 #[tokio::main]
 async fn main(){
-
-    let app = Command::new("gpad-cli")
-        .version("0.0.1")
-        .author("noko1024")
-        .about("GitPATLink CLI Client")
-        .subcommand(Command::new("add")
-            .about("PAT additions or updates")
-            .arg(Arg::new("student ID Number")
-                .help("pXXXXXX or sXXXXXX")
-                .required(true)
-            )
-            .arg(Arg::new("password")
-                .help("Your password")
-                .required(true)
-            )
-            .arg(Arg::new("Pasonal Access Token")
-                .help("Your Pasonal Access Token")
-                .required(true)
-            )
-        )
-        .subcommand(Command::new("get")
-            .about("PAT additions and updates")
-            .arg(Arg::new("student ID number")
-            .help("pXXXXXX or sXXXXXX")
-                .required(true)
-            )
-            .arg(Arg::new("password")
-                .help("Your password")
-                .required(true)
-            )
-        )
-        .subcommand(Command::new("remove")
-        .about("PAT additions and updates")
-        .arg(Arg::new("student ID number")
-        .help("pXXXXXX or sXXXXXX")
-            .required(true)
-        )
-        .arg(Arg::new("password")
-            .help("Your password")
-            .required(true)
-        )
-    );
     // 引数を解析
-    let matches = app.get_matches();
+    let matches = cli::build_cli().get_matches();
 
     // subサブコマンドの解析結果を取得
-    if let Some(ref matches) = matches.subcommand_matches("add") {
+    if let Some(ref matches) = matches.subcommand_matches("comp"){
+        let shell_name = matches.value_of("Shell Name").unwrap().to_string();
+
+        if shell_name == "bash" {
+            generate(Bash, &mut cli::build_cli(), "gpad-cli", &mut io::stdout());
+        }
+        else if shell_name == "zsh" {
+            generate(Zsh, &mut cli::build_cli(), "gpad-cli", &mut io::stdout());
+        }
+        else if shell_name == "powershell" {
+            generate(PowerShell, &mut cli::build_cli(), "gpad-cli", &mut io::stdout());
+        }
+        else if shell_name == "fish" {
+            generate(Fish, &mut cli::build_cli(), "gpad-cli", &mut io::stdout());
+        }
+        else if shell_name == "elvish" {
+            generate(Elvish, &mut cli::build_cli(), "gpad-cli", &mut io::stdout());
+        }
+
+    }
+    else if let Some(ref matches) = matches.subcommand_matches("add") {
         let id = matches.value_of("student ID Number").unwrap().to_string();
         let password = matches.value_of("password").unwrap().to_string();
         let token = matches.value_of("Pasonal Access Token").unwrap().to_string();
-        println!("{}",id);
-        println!("{}",password);
-        println!("{}",token);
+        //Pasonal Access Tokenを passwordで暗号化
         let enc_token = _encrypt(&password,token);
-        println!("{}",enc_token);
-        println!("enc");
-        //let dec_token = _decrypt(&password, enc_token);
-        let mut hasher = Sha256::new();
-        hasher.input_str(&password.to_string());
-        let hex_password = hasher.result_str();
-        //println!("{}",dec_token);
-        //println!("dec");
-        let res = _http_post("/link/api/add",vec![id.clone(),hex_password,enc_token]).await;
+        //sha256でpasswordをハッシュ化
+        let hash_password = hash(password,10).unwrap();
+
+        let res = _http_post("/link/api/add",vec![id.clone(),hash_password,enc_token]).await;
         println!("{}",res)
         
 
@@ -86,12 +60,9 @@ async fn main(){
     else if let Some(ref matches) = matches.subcommand_matches("get") {
         let id = matches.value_of("student ID number").unwrap().to_string();
         let password = matches.value_of("password").unwrap().to_string();
-        let mut hasher = Sha256::new();
-        hasher.input_str(&password.to_string());
-        let hex_password = hasher.result_str();
-        println!("{}",id);
-        println!("{}",password);
-        let raw_token = _http_post("/link/api/get",vec![id.clone(),hex_password]).await;
+        let hash_password = hash(&password, 10).unwrap();
+        println!("{}",hash_password);
+        let raw_token = _http_post("/link/api/get",vec![id.clone(),hash_password]).await;
         let token = _decrypt(&password, raw_token);
         println!("{}",token);
     }
@@ -101,6 +72,7 @@ async fn main(){
         println!("{}",id);
         println!("{}",password);
     }
+
 
     fn _encrypt(password:&str, source:String) -> String  {
         const PASSWORD_SIZE: usize = 32;
@@ -119,10 +91,10 @@ async fn main(){
             let writer = AesWriter::new(&mut encrypted, encryptor);
             writer.unwrap().write_all(source.as_bytes());
         }
-
         return base64::encode(encrypted);
     }
     
+
     fn _decrypt(password:&str, source:String)-> String{
         let decoded:Vec<u8> = base64::decode(source).unwrap();
         const PASSWORD_SIZE: usize = 32;
@@ -136,11 +108,12 @@ async fn main(){
             key_array[i] = key[i];
         }
         let decryptor = AesSafe256Decryptor::new(&key_array);
-        let mut reader = AesReader::new(Cursor::new(decoded), decryptor);
+        let reader = AesReader::new(Cursor::new(decoded), decryptor);
         let mut decrypted = Vec::new();
         reader.unwrap().read_to_end(&mut decrypted);
         return String::from_utf8(decrypted).unwrap();
     }
+
 
     async fn _http_post(api_end_point:&str, user_auth_info:Vec<String>) -> String{
         let api_root = "http://localhost:4000".to_string();
@@ -186,9 +159,10 @@ async fn main(){
             println!("{}",res_text);
             process::exit(1)
         }
-        else if status_code == 403{
+        else if status_code == 401{
             println!("Error:Auth Error");
-            println!("Hint:Is your password wrong?");
+            println!("Hint:1. Is your password wrong?");
+            println!("Hint:2. Possibility that the authentication information has not been registered.");
             let res = response_data.text().await;
             let res_text = res.unwrap();
             println!("{}",status_code);
